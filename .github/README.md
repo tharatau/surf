33396455eae93120b63c7c4b5884de3bd0513f06
Ayushman Chhabra <ayushmxn@outlook.com>
2022-07-21 00:32:19 -0400

    feat(chrome): initialise window with `gtk3`

--- .github/README.md
+++ .github/README.md
@@ -1,3 +1,3 @@
 # Pahuch
 
-Pahuch means access in Hindi. An attempt to create a browser.
+Pahuch means access in Hindi
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,23 @@
+use gtk::prelude::*;
+use gtk::{Application, ApplicationWindow};
+
 fn main() {
-    println!("Hello, world!");
+    let app = Application::builder()
+        .application_id("org.pahuch.browser")
+        .build();
+
+    app.connect_activate(|app| {
+        // We create the main window.
+        let win = ApplicationWindow::builder()
+            .application(app)
+            .default_width(500)
+            .default_height(500)
+            .title("Pahuch")
+            .build();
+
+        // Don't forget to make all widgets visible.
+        win.show_all();
+    });
+
+    app.run();
 }
