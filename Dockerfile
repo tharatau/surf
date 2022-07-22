87471c444a086bda46ab24631aa4b98264c753b0
Ayushman Chhabra <ayushmxn@outlook.com>
2022-07-22 16:14:39 -0400

    chore(ci): add multi stage docker build

---
+++ Dockerfile
@@ -0,0 +1,9 @@
+FROM rust AS RUNTIME
+WORKDIR ./rust
+COPY . .
+RUN cargo build
+
+FROM ubuntu:22.04 AS OPERATING_SYSTEM
+COPY --from=RUNTIME ./rust/target/debug/pahuch ./bin/pahuch
+WORKDIR ./bin
+CMD pahuch
--- src/main.rs
+++ src/main.rs
@@ -1,23 +1,3 @@
-use gtk::prelude::*;
-use gtk::{Application, ApplicationWindow};
-
 fn main() {
-    let app = Application::builder()
-        .application_id("org.pahuch.browser")
-        .build();
-
-    app.connect_activate(|app| {
-        // We create the main window.
-        let win = ApplicationWindow::builder()
-            .application(app)
-            .default_width(500)
-            .default_height(500)
-            .title("Pahuch")
-            .build();
-
-        // Don't forget to make all widgets visible.
-        win.show_all();
-    });
-
-    app.run();
+    println!("Hello, World!")
 }
