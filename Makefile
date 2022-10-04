b53f14c3ad3789310a5d9276a25a5bed706eb132
Ayushman Chhabra <ayushmxn@outlook.com>
2022-10-04 00:34:23 -0400

    try linking cURL

--- Makefile
+++ Makefile
@@ -3,4 +3,4 @@ clean:
 	rm surf
 
 surf: surf.c
-	clang surf.c -o surf
+	clang surf.c -o surf -I ./git_modules/curl/include/curl
--- surf.c
+++ surf.c
@@ -1,6 +1,19 @@
 #include <stdio.h>
-#include <stdlib.h>
+#include <curl/curl.h>
 
-int main(int argc, char *argv[]) {
+int main()
+{
+    CURL *curl;
+    char url[] =
+        "https://c-for-dummies.com/curl_test.txt";
+
+    curl = curl_easy_init();
+    if (curl) {
+        CURLcode res;
+        curl_easy_setopt(curl, CURLOPT_URL, "https://example.com");
+        res = curl_easy_perform(curl);
+        curl_easy_cleanup(curl);
+    }
+
     return 0;
 }
