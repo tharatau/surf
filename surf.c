75d22470dd64cdb7c421ef5e6e68ac3de5a5a5b7
Ayushman Chhabra <ayushmxn@outlook.com>
2022-10-09 14:14:58 -0400

    warn message if more than 1 argument given by user

--- surf.c
+++ surf.c
@@ -1,9 +1,25 @@
 #include "surf.h"
 
+int validate(int argc, char *argv[]) {
+    if (argc == 1) {
+        fprintf(stderr, "[ ERROR ] Received 0 arguments but expected 1. The `uri` parameter is missing.\n");
+        return 1;
+    }
+
+    if (argc > 2) {
+        fprintf(stdout, "[ WARN ] Received more than 1 argument but expected 1.\n");
+        fprintf(stdout, "[ INFO ] Validating first argument only.\n");
+    }
+
+    return 0;
+
+}
+
 int main(int argc, char *argv[])
 {
-    if (argc == 1) {
-        fprintf(stderr, "Error: Received 0 arguments but expected 1. The `uri` parameter is missing.\n");
-        exit(1);
+    int e = validate(argc, argv);
+    if (e != 0) {
+        return 1;
     }
+    return 0;
 }
